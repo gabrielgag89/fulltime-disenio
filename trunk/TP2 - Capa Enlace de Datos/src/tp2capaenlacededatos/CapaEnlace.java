51f3d4e77d13b411bf6fbb49cbba2a129e7b3d90
gabrielgag89@gmail.com <gabrielgag89@gmail.com@a671dc1e-94f1-10d5-c008-1e1a2ba96bbb>
2011-10-21 16:20:23 +0000

    Capa enlace (métodos)

--- trunk/TP2 - Capa Enlace de Datos/src/tp2capaenlacededatos/CapaEnlace.java
+++ trunk/TP2 - Capa Enlace de Datos/src/tp2capaenlacededatos/CapaEnlace.java
@@ -8,5 +8,20 @@ package tp2capaenlacededatos;
  * @author gabriel
  */
 public class CapaEnlace {
+   public static final char esMsj = 10;
+   public static final char esAck = 20;
    
+   public void enviarPaquete(Trama trama){
+      
+   }
+   
+   public void desdeFisica(Trama trama){
+      
+   }
+   
+   public char calcSumVerif(Trama t){
+      long suma = t.getByteInicio() + t.getNumSec() + t.getDato() + t.getByteFin();
+      
+      return (char) suma;
+   }
 } // fin de la clase CapaEnlace
