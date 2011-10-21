bb417ddb0cf4004721c2a65834b6afb4c65674a9
gabrielgag89@gmail.com <gabrielgag89@gmail.com@a671dc1e-94f1-10d5-c008-1e1a2ba96bbb>
2011-10-21 16:27:20 +0000

    Capa enlace (singleton)

--- TP2 - Capa Enlace de Datos/src/tp2capaenlacededatos/CapaEnlace.java
+++ TP2 - Capa Enlace de Datos/src/tp2capaenlacededatos/CapaEnlace.java
@@ -8,8 +8,17 @@ package tp2capaenlacededatos;
  * @author gabriel
  */
 public class CapaEnlace {
+   private static CapaEnlace capaEnlace;
+   
    public static final char esMsj = 10;
    public static final char esAck = 20;
+   
+   public static CapaEnlace getInstancia(){
+      if(capaEnlace == null)
+         capaEnlace = new CapaEnlace();
+      
+      return capaEnlace;
+   }
    
    public void enviarPaquete(Trama trama){
       
