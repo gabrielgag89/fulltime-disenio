40bde7c7b1b076adcfa227a4d0db63fe754ddb4c
gabrielgag89@gmail.com <gabrielgag89@gmail.com@a671dc1e-94f1-10d5-c008-1e1a2ba96bbb>
2011-10-21 15:44:44 +0000

    Trama terminada

--- trunk/TP2 - Capa Enlace de Datos/src/tp2capaenlacededatos/Trama.java
+++ trunk/TP2 - Capa Enlace de Datos/src/tp2capaenlacededatos/Trama.java
@@ -32,4 +32,63 @@ public class Trama {
     * Bandera de fin.
     */
    private char byteFin = '\n';
+   
+   public Trama(){
+      
+   }
+
+   public Trama(char numSec, char msj_ack, char dato, char sumVerif){
+      this.numSec = numSec;
+      this.msj_ack = msj_ack;
+      this.dato = dato;
+      this.sumVerif = sumVerif;
+   }
+   
+   public char getByteInicio() {
+      return byteInicio;
+   }
+
+   public void setByteInicio(char byteInicio) {
+      this.byteInicio = byteInicio;
+   }
+
+   public char getNumSec() {
+      return numSec;
+   }
+
+   public void setNumSec(char numSec) {
+      this.numSec = numSec;
+   }
+
+   public char getMsj_ack() {
+      return msj_ack;
+   }
+
+   public void setMsj_ack(char msj_ack) {
+      this.msj_ack = msj_ack;
+   }
+
+   public char getDato() {
+      return dato;
+   }
+
+   public void setDato(char dato) {
+      this.dato = dato;
+   }
+
+   public char getSumVerif() {
+      return sumVerif;
+   }
+
+   public void setSumVerif(char sumVerif) {
+      this.sumVerif = sumVerif;
+   }
+
+   public char getByteFin() {
+      return byteFin;
+   }
+
+   public void setByteFin(char byteFin) {
+      this.byteFin = byteFin;
+   }
 } // fin de la clase Trama
