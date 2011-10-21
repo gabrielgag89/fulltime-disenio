292d12dd2b2c48dbf62c95c55f371e1ca459f601
gabrielgag89@gmail.com <gabrielgag89@gmail.com@a671dc1e-94f1-10d5-c008-1e1a2ba96bbb>
2011-10-21 15:28:11 +0000

    Trama

---
+++ trunk/TP2 - Capa Enlace de Datos/src/tp2capaenlacededatos/Trama.java
@@ -0,0 +1,15 @@
+// Trama: Trama.java
+// 
+package tp2capaenlacededatos;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Trama {
+   private char byteInicio;
+   private char numSec;
+   private char dato;
+   private char byteFin;
+   
+} // fin de la clase Trama
