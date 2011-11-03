7726087cf32a969c19a34b1846470a7ee42713b8
matiasgaston.campos19@gmail.com <matiasgaston.campos19@gmail.com@a671dc1e-94f1-10d5-c008-1e1a2ba96bbb>
2011-11-03 11:53:20 +0000

    DetalleFicha

--- Proyecto/Clínica/src/persistencia/proxy/DetalleFicha.java
+++ Proyecto/Clínica/src/persistencia/proxy/DetalleFicha.java
@@ -1,43 +1,16 @@
 package persistencia.proxy;
-
 /**
- * @author Cristian
- * @version 1.0
- * @created 22-oct-2011 07:16:37 p.m.
+ *
+ * @author Matías
  */
-public class DetalleFicha {
+public interface DetalleFicha{
 
-	private Date fechaAlta;
-	public ServicioEspecial m_ServicioEspecial;
-
-	public DetalleFicha(){
-
-	}
-
-	public void finalize() throws Throwable {
-
-	}
-	public Date getFechaAlta(){
-		return FechaAlta;
-	}
-
-	/**
-	 * 
-	 * @param newVal
-	 */
-	public void setFechaAlta(Date newVal){
-		FechaAlta = newVal;
-	}
-
-	public ServicioEspecial getServicioEspecial(){
-		return null;
-	}
-
-	/**
-	 * 
-	 * @param servicioEspecial
-	 */
-	public void setServicioEspecial(ServicioEspecial servicioEspecial){
-
-	}
-}//end DetalleFicha
+    public FichaInternacion getFichaInternacion();
+    
+    public void setFichaInternacion(FichaInternacion fichaInternacion);
+    
+    public ServicioEspecial getServicioEspecial();
+    
+    public void setServicioEspecial(ServicioEspecial servicioEspecial);
+    
+}
