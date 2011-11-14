8b419617f01abeafeb5334a45d715f9d88e57004
gabrielgag89@gmail.com <gabrielgag89@gmail.com@a671dc1e-94f1-10d5-c008-1e1a2ba96bbb>
2011-11-14 23:58:16 +0000

    Dump completo y arreglo en la columna oidfactura_os de ficha_internacion (ahora es nulleable)

--- Proyecto/Base de Datos (MySQL)/clinica.sql
+++ Proyecto/Base de Datos (MySQL)/clinica.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v9.02 
-MySQL - 5.5.13 : Database - clinica
+MySQL - 5.5.17 : Database - clinica
 *********************************************************************
 */
 
@@ -16,17 +16,6 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`clinica` /*!40100 DEFAULT CHARACTER SET
 
 USE `clinica`;
 
-/*Table structure for table `banco` */
-
-DROP TABLE IF EXISTS `banco`;
-
-CREATE TABLE `banco` (
-  `oidbanco` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `codigo_banco` int(11) NOT NULL,
-  `nombre_banco` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`oidbanco`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 /*Table structure for table `cama` */
 
 DROP TABLE IF EXISTS `cama`;
@@ -39,8 +28,8 @@ CREATE TABLE `cama` (
   PRIMARY KEY (`oidcama`),
   KEY `fk_cama_habitacion1` (`oidhabitacion`),
   KEY `fk_cama_estado_cama1` (`oidestado_cama`),
-  CONSTRAINT `fk_cama_habitacion1` FOREIGN KEY (`oidhabitacion`) REFERENCES `habitacion` (`oidhabitacion`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_cama_estado_cama1` FOREIGN KEY (`oidestado_cama`) REFERENCES `estado_cama` (`oidestado_cama`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_cama_estado_cama1` FOREIGN KEY (`oidestado_cama`) REFERENCES `estado_cama` (`oidestado_cama`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cama_habitacion1` FOREIGN KEY (`oidhabitacion`) REFERENCES `habitacion` (`oidhabitacion`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Table structure for table `convenio` */
@@ -58,9 +47,9 @@ CREATE TABLE `convenio` (
   KEY `fk_convenio_plan1` (`oidplan`),
   KEY `fk_convenio_prestacion1` (`oidprestacion`),
   KEY `fk_convenio_coseguro1` (`oidcoseguro`),
+  CONSTRAINT `fk_convenio_coseguro1` FOREIGN KEY (`oidcoseguro`) REFERENCES `coseguro` (`oidcoseguro`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_convenio_plan1` FOREIGN KEY (`oidplan`) REFERENCES `plan` (`oidplan`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_convenio_prestacion1` FOREIGN KEY (`oidprestacion`) REFERENCES `prestacion` (`oidprestacion`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_convenio_coseguro1` FOREIGN KEY (`oidcoseguro`) REFERENCES `coseguro` (`oidcoseguro`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_convenio_prestacion1` FOREIGN KEY (`oidprestacion`) REFERENCES `prestacion` (`oidprestacion`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Table structure for table `coseguro` */
@@ -139,16 +128,6 @@ CREATE TABLE `estado_factura_cliente` (
   PRIMARY KEY (`oidestado_factura_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-/*Table structure for table `estado_factura_os` */
-
-DROP TABLE IF EXISTS `estado_factura_os`;
-
-CREATE TABLE `estado_factura_os` (
-  `oidestado_factura_os` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre_estado_factura_os` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`oidestado_factura_os`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 /*Table structure for table `estado_ficha_internacion` */
 
 DROP TABLE IF EXISTS `estado_ficha_internacion`;
@@ -176,23 +155,6 @@ CREATE TABLE `factura_cliente` (
   CONSTRAINT `fk_factura_cliente_ficha_internacion1` FOREIGN KEY (`oidficha_internacion`) REFERENCES `ficha_internacion` (`oidficha_internacion`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-/*Table structure for table `factura_os` */
-
-DROP TABLE IF EXISTS `factura_os`;
-
-CREATE TABLE `factura_os` (
-  `oidfactura_os` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `numero_factura_os` int(11) NOT NULL,
-  `fecha_emision` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `oidestado_factura_os` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `oidobra_social` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`oidfactura_os`),
-  KEY `fk_factura_os_estado_factura_os` (`oidestado_factura_os`),
-  KEY `fk_factura_os_obra_social1` (`oidobra_social`),
-  CONSTRAINT `fk_factura_os_estado_factura_os` FOREIGN KEY (`oidestado_factura_os`) REFERENCES `estado_factura_os` (`oidestado_factura_os`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_factura_os_obra_social1` FOREIGN KEY (`oidobra_social`) REFERENCES `obra_social` (`oidobra_social`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 /*Table structure for table `ficha_internacion` */
 
 DROP TABLE IF EXISTS `ficha_internacion`;
@@ -205,7 +167,7 @@ CREATE TABLE `ficha_internacion` (
   `oidestado_ficha_internacion` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `oidcama` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `oidpaciente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `oidfactura_os` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `oidfactura_os` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`oidficha_internacion`),
   KEY `fk_ficha_internacion_prestacion1` (`oidprestacion`),
   KEY `fk_ficha_internacion_estado_ficha_internacion1` (`oidestado_ficha_internacion`),
@@ -217,16 +179,6 @@ CREATE TABLE `ficha_internacion` (
   CONSTRAINT `fk_ficha_internacion_cama1` FOREIGN KEY (`oidcama`) REFERENCES `cama` (`oidcama`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_ficha_internacion_paciente1` FOREIGN KEY (`oidpaciente`) REFERENCES `paciente` (`oidpaciente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_ficha_internacion_factura_os1` FOREIGN KEY (`oidfactura_os`) REFERENCES `factura_os` (`oidfactura_os`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-/*Table structure for table `forma_pago` */
-
-DROP TABLE IF EXISTS `forma_pago`;
-
-CREATE TABLE `forma_pago` (
-  `oidforma_pago` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre_forma_pago` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`oidforma_pago`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Table structure for table `habitacion` */
@@ -243,25 +195,6 @@ CREATE TABLE `habitacion` (
   KEY `fk_habitacion_tipo_habitacion1` (`oidtipo_habitacion`),
   CONSTRAINT `fk_habitacion_sector1` FOREIGN KEY (`oidsector`) REFERENCES `sector` (`oidsector`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_habitacion_tipo_habitacion1` FOREIGN KEY (`oidtipo_habitacion`) REFERENCES `tipo_habitacion` (`oidtipo_habitacion`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-/*Table structure for table `monto` */
-
-DROP TABLE IF EXISTS `monto`;
-
-CREATE TABLE `monto` (
-  `oidmonto` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `monto` double NOT NULL,
-  `oidrecibo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `oidforma_pago` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `oidtarjeta_aceptada` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`oidmonto`),
-  KEY `fk_monto_recibo1` (`oidrecibo`),
-  KEY `fk_monto_forma_pago1` (`oidforma_pago`),
-  KEY `fk_monto_tarjeta_aceptada1` (`oidtarjeta_aceptada`),
-  CONSTRAINT `fk_monto_recibo1` FOREIGN KEY (`oidrecibo`) REFERENCES `recibo` (`oidrecibo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_monto_forma_pago1` FOREIGN KEY (`oidforma_pago`) REFERENCES `forma_pago` (`oidforma_pago`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_monto_tarjeta_aceptada1` FOREIGN KEY (`oidtarjeta_aceptada`) REFERENCES `tarjeta_aceptada` (`oidtarjeta_aceptada`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Table structure for table `obra_social` */
@@ -357,20 +290,6 @@ CREATE TABLE `servicio_especial` (
   PRIMARY KEY (`oidservicio_especial`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-/*Table structure for table `tarjeta_aceptada` */
-
-DROP TABLE IF EXISTS `tarjeta_aceptada`;
-
-CREATE TABLE `tarjeta_aceptada` (
-  `oidtarjeta_aceptada` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `codigo_tarjeta_aceptada` int(11) NOT NULL,
-  `nombre_tarjeta_aceptada` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `oidbanco` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`oidtarjeta_aceptada`),
-  KEY `fk_tarjeta_aceptada_banco1` (`oidbanco`),
-  CONSTRAINT `fk_tarjeta_aceptada_banco1` FOREIGN KEY (`oidbanco`) REFERENCES `banco` (`oidbanco`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 /*Table structure for table `tipo_habitacion` */
 
 DROP TABLE IF EXISTS `tipo_habitacion`;
@@ -394,20 +313,6 @@ CREATE TABLE `tipo_prestacion` (
   PRIMARY KEY (`oidtipo_prestacion`),
   KEY `fk_tipo_prestacion_tipo_habitacion1` (`oidtipo_habitacion`),
   CONSTRAINT `fk_tipo_prestacion_tipo_habitacion1` FOREIGN KEY (`oidtipo_habitacion`) REFERENCES `tipo_habitacion` (`oidtipo_habitacion`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-/*Table structure for table `vigencia_tarjeta` */
-
-DROP TABLE IF EXISTS `vigencia_tarjeta`;
-
-CREATE TABLE `vigencia_tarjeta` (
-  `oidvigencia_tarjeta` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `fecha_inicio` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `fecha_fin` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `oidtarjeta_aceptada` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`oidvigencia_tarjeta`),
-  KEY `fk_vigencia_tarjeta_tarjeta_aceptada1` (`oidtarjeta_aceptada`),
-  CONSTRAINT `fk_vigencia_tarjeta_tarjeta_aceptada1` FOREIGN KEY (`oidtarjeta_aceptada`) REFERENCES `tarjeta_aceptada` (`oidtarjeta_aceptada`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
