8f481e2ce6a0402e85aef07c6f899a6bbc74101c
cristiancroc@gmail.com <cristiancroc@gmail.com@a671dc1e-94f1-10d5-c008-1e1a2ba96bbb>
2011-11-26 15:22:18 +0000

    subido stub para consultas

---
+++ trunk/Proyecto/stub/src/stub/Stub.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stub;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Stub {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      new IUStub().setVisible(true);
+   }// TODO code application logic here
+}
