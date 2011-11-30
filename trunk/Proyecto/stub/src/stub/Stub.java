df43a18083508eda9e957ae6f741b2cd34e3ff56
gabrielgag89@gmail.com <gabrielgag89@gmail.com@a671dc1e-94f1-10d5-c008-1e1a2ba96bbb>
2011-11-30 16:01:24 +0000

    Arreglos en los casos de pruebas y tablas

--- trunk/Proyecto/stub/src/stub/Stub.java
+++ trunk/Proyecto/stub/src/stub/Stub.java
@@ -1,18 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package stub;
 
-/**
- *
- * @author Cristian
- */
 public class Stub {
-
-   /**
-    * @param args the command line arguments
-    */
+   
    public static void main(String[] args) {
       new IUStub().setVisible(true);
    }// TODO code application logic here
